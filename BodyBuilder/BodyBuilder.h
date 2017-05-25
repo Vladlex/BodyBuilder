4aff1d5e9476bee46435bb9f228d021271637e3a
Vladlex <vladlexion@gmail.com>
2017-05-25 11:51:05 +0300

    Initial Commit

---
+++ BodyBuilder/BodyBuilder.h
@@ -0,0 +1,19 @@
+//
+//  BodyBuilder.h
+//  BodyBuilder
+//
+//  Created by Aleksei Gordeev on 25/05/2017.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BodyBuilder.
+FOUNDATION_EXPORT double BodyBuilderVersionNumber;
+
+//! Project version string for BodyBuilder.
+FOUNDATION_EXPORT const unsigned char BodyBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BodyBuilder/PublicHeader.h>
+
+
